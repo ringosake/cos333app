1dd3e83791f986e199641dc33f3a40a641d773be
ringosake <cissyzed@gmail.com>
2013-04-03 00:30:50 -0400

    main menu page: the layer-list doesnt completely work

--- src/com/example/cos333app/ImageAdapter.java
+++ src/com/example/cos333app/ImageAdapter.java
@@ -54,6 +54,7 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.sample_0, R.drawable.sample_1,
             R.drawable.sample_2, R.drawable.sample_3,
             R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.layer
     };
 }
