8b34f846fd178a43c9b8351bdfa58b547fe3c3c7
ringosake <cissyzed@gmail.com>
2013-04-02 22:53:22 -0400

    gridview hello world

--- src/com/example/cos333app/MainActivity.java
+++ src/com/example/cos333app/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.cos333app;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	    setContentView(R.layout.activity_main);
+
+	    GridView gridview = (GridView) findViewById(R.id.gridview);
+	    gridview.setAdapter(new ImageAdapter(this));
+
+	    /*gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });*/
 	}
 
 	@Override
