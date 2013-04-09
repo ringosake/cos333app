782edd35173c62758bd60ffbeb9da1e940a479a4
ringosake <cissyzed@gmail.com>
2013-04-09 06:22:19 -0400

    gui: press the grid items! goes to same dummy map view now!

--- src/com/example/cos333app/MainActivity.java
+++ src/com/example/cos333app/MainActivity.java
@@ -20,16 +20,22 @@ public class MainActivity extends Activity {
 	    GridView gridview = (GridView) findViewById(R.id.gridview);
 	    gridview.setAdapter(new ImageAdapter(this));
 
-	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	    /*gridview.setOnItemClickListener(new OnItemClickListener() {
 	    	@Override
 	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 	            Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_SHORT).show();
 	            //Intent myIntent = new Intent(this, MapActivity.class);
 	            //startActivityForResult(myIntent, 0);
 	        }
-	    });
+	    });*/
 	}
 
+	public void openMap(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, MapActivity.class);
+	    startActivity(intent);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
