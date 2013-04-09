52c39cb131562c32fa00a5ef61fa8e35f3aaa7f8
ringosake <cissyzed@gmail.com>
2013-04-09 07:39:03 -0400

    some fixes

--- src/com/example/cos333app/MapActivity.java
+++ src/com/example/cos333app/MapActivity.java
@@ -2,7 +2,9 @@ package com.example.cos333app;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MapActivity extends Activity {
 
@@ -19,4 +21,9 @@ public class MapActivity extends Activity {
 		return true;
 	}
 
+	public void goHome(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, MainActivity.class);
+	    startActivity(intent);
+	}
 }
