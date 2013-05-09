8acc1f26e359bb9c198d90633b86a705b4e3dd19
adai <angelaxai17@gmail.com>
2013-05-09 15:06:33 -0400

    marker colours and field of view bounds

--- src/com/example/cos333app/MapActivity.java
+++ src/com/example/cos333app/MapActivity.java
@@ -1,7 +1,6 @@
 package com.example.cos333app;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 import library.UserFunctions;
 
@@ -10,9 +9,6 @@ import org.json.JSONObject;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
@@ -20,6 +16,7 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.util.SparseIntArray;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,25 +24,34 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.model.CameraPosition;
 
 public class MapActivity extends FragmentActivity {
 	
+	// maps info
 	private GoogleMap mMap;
     private UiSettings mUiSettings;
-    GPSTracker gps;
-    double latitude = 0, longitude = 0;
-    private int updatetime = 5000; // 5 seconds
+    private GPSTracker gps;
+    private double latitude = 0, longitude = 0;
+    private LatLngBounds bounds = null;
     private Handler handler;
-    private List<Marker> markers;
-    private String email, token; // to identify user
+    private LinkedList<Marker> markers;
+    private LinkedList<LatLng> locations;
+    private SparseIntArray userToIndex;
+    private static int updatetime = 5000; // 5 seconds
+    private static double mindist = 5; // don't update unless 5m difference
+    
+    // user, group identification
+	private String email, token; // to identify user
+    private int uid = 101; // !!!!!! change to get from shared prefs
     private int groupid = 1;
     
-    UserFunctions userFunctions;
-    LocationManager locationManager;
-    Geocoder geocoder;
+    private UserFunctions userFunctions;
     
     private static String KEY_STATUS = "status";
     private static String VAL_SUCCESS = "success";
@@ -53,6 +59,9 @@ public class MapActivity extends FragmentActivity {
     private static String KEY_NAME = "user_name";
     private static String KEY_LAT = "latitude";
     private static String KEY_LONG = "longitude";
+    private static String KEY_UID = "user_id";
+    private static float SELF_COLOUR = BitmapDescriptorFactory.HUE_RED;
+    private static float OTHERS_COLOUR = BitmapDescriptorFactory.HUE_AZURE;
     
     Runnable statusChecker = new Runnable() {
     	@Override
@@ -93,14 +102,9 @@ public class MapActivity extends FragmentActivity {
 		userFunctions = new UserFunctions();
 		
 		handler = new Handler();
-        markers = new ArrayList<Marker>();
-        locationManager = (LocationManager)this.getSystemService(LOCATION_SERVICE);
-        geocoder = new Geocoder(this);
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location != null) {
-          latitude = location.getLatitude();
-          longitude = location.getLongitude();
-        }
+        markers = new LinkedList<Marker>();
+        locations = new LinkedList<LatLng>();
+        userToIndex = new SparseIntArray();
         
         setUpMapIfNeeded();
         startRepeatingTask();
@@ -152,9 +156,8 @@ public class MapActivity extends FragmentActivity {
             latitude = gps.getLatitude();
             longitude = gps.getLongitude();
             // send location to database
-            if (email != null && token != null) {
+            if (email != null && token != null)
             	updateMyLocation();
-            }
             else 
             	Log.e("USERINFO", "email/token NULL");
             
@@ -172,13 +175,48 @@ public class MapActivity extends FragmentActivity {
         mUiSettings.setScrollGesturesEnabled(true);
         mUiSettings.setZoomGesturesEnabled(true);
         mUiSettings.setZoomControlsEnabled(false);
+        mMap.setOnCameraChangeListener(new OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition arg0) {
+                // Move camera.
+                mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 80));
+                // Remove listener to prevent position reset on camera move.
+                mMap.setOnCameraChangeListener(null);
+            }
+        });
     }
 	
+	/* focus view on (lat, lng) */
 	private void moveCameraView(double lat, double lng) {
+		Log.d("MAPACTIVITY", "setting camera to latlng: (" +lat + ", " + lng + ")");
 		CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 15);
         mMap.moveCamera(cameraUpdate);
 	}
+	/* view all locations */
+	private void moveCameraView() {
+		Log.d("MAPACTIVITY", "setting camera to bounds: " + bounds);
+		if (bounds != null) mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+	}
+	/* compute latlngbounds from users */
+	private void computeBounds(int[] users) {
+		boolean first = true;
+		int index;
+		for (int i = 0; i < users.length; i++) {
+			index = userToIndex.get(users[i], -1);
+			if (index > 0) {
+				if (first) {
+					bounds = new LatLngBounds(locations.get(index), locations.get(index));
+					first = false;
+				}
+				else
+					bounds.including(locations.get(index));
+			}
+			else
+				Log.e("MAPACTIVITY", "cannot find user " + users[i] + " in stored group info");
+		}
+	}
 	
+	/* update all locations from database */
 	private void updateAllLocations() {
 		// query from database 
     	JSONObject jsonret = userFunctions.retrieveAllLocations(email, token, groupid);
@@ -196,12 +234,17 @@ public class MapActivity extends FragmentActivity {
     			}
     			double lat = 0, lng = 0;
     			String name = " ";
+    			int userid = 0;
+    			float colour;
     			// clear markers
-    			for (int m = 0; m < markers.size(); m++) {
+    			for (int m = 0; m < markers.size(); m++)
     				markers.get(m).remove();
-    			}
     			markers.clear();
+    			locations.clear();
+    			userToIndex.clear();
+				bounds = new LatLngBounds(new LatLng(latitude, longitude), new LatLng(latitude, longitude));
     			// loop through all locations
+				int index = 0;
     			for (int u = 0; u < nusers; u++) {
     				JSONObject jsonuser = null;
     				if (jsonret.getString(Integer.toString(u)) != null) {
@@ -209,13 +252,21 @@ public class MapActivity extends FragmentActivity {
         				if (jsonuser.getString(KEY_LAT) != null) { lat = jsonuser.getDouble(KEY_LAT); }
         				if (jsonuser.getString(KEY_LONG) != null) { lng = jsonuser.getDouble(KEY_LONG); }
         				if (jsonuser.getString(KEY_NAME) != null) { name = jsonuser.get(KEY_NAME).toString(); }
+        				if (jsonuser.getString(KEY_UID) != null) { userid = jsonuser.getInt(KEY_UID); } 
+        				if (userid != uid) { colour = OTHERS_COLOUR; }
+        				else               { colour = SELF_COLOUR;   }
         				Log.d("RETRIEVE", name + " at (" + lat + ", " + lng + ")");
+        				userToIndex.put(userid, index);
+        				locations.add(new LatLng(lat, lng));
+                        bounds = bounds.including(new LatLng(lat, lng));
         				Marker m = mMap.addMarker(new MarkerOptions()
         										.position(new LatLng(lat, lng))
-        										.title(name));//,
-        										//.icon(BitmapDescriptorFactory.defaultMarker(0)));
+        										.title(name)
+        										.icon(BitmapDescriptorFactory.defaultMarker(colour)));
         				m.setVisible(true);
+        				if (userid == uid) m.showInfoWindow();
         				markers.add(m);
+        				index++;
     				}
     			}
     		}
