08d7f42ab0c7b0ab7aae46a99977e13dd8753056
ringosake <cissyzed@gmail.com>
2013-05-09 14:40:11 -0400

    autocomplete client side works

--- src/com/example/cos333app/NewGroupActivity.java
+++ src/com/example/cos333app/NewGroupActivity.java
@@ -7,6 +7,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.io.OutputStream;
 import java.net.URL;
 
@@ -33,16 +37,21 @@ import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
 import android.support.v4.app.DialogFragment;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 public class NewGroupActivity extends Activity {
 
@@ -60,44 +69,79 @@ public class NewGroupActivity extends Activity {
 	ImageView imgLogo;
 	private ImageView image;
 	private String groupID = "12"; //TODO: get the real one somehow
-	
+	TextView errorMsg;
+	private Thread thread; 
 	// for contact autocomplete
 	MultiAutoCompleteTextView contactView;
-	public ArrayList<String> c_Name = new ArrayList<String>();
-	public ArrayList<ArrayList<String>> c_Number = new ArrayList<ArrayList<String>>();
-	String[] name_Val=null;
+	Map<String, String>[] people=null;
+	public ArrayList<ContactMap> mPeopleList = new ArrayList<ContactMap>();
+	private ArrayList<String> mNames = new ArrayList<String>();
+	private SimpleAdapter mAdapter;
+	boolean[] isSelected;
 	
-	public void readContacts(){
-        ContentResolver cr = getContentResolver();
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-               null, null, null, null);
-        
-        if (cur.getCount() > 0) {
-           while (cur.moveToNext()) {
-               String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-               String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-               if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-            	   c_Name.add(name);
-            	   ArrayList<String> nums = new ArrayList<String>();
-                   // get the phone number
-                   Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
-                                          ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
-                                          new String[]{id}, null);
-                   while (pCur.moveToNext()) {
-                         String phone = pCur.getString(
-                                pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                         nums.add(phone);
-                   }
-                   c_Number.add(nums);
-                   pCur.close();
-               }
-           }
-      }
-   }
+	class ContactMap extends HashMap<String, String> { 
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public String toString() { 
+			return this.get("Name") + " <" + this.get("Phone") + ">"; 
+		} 
+	}
+	
+	public void readContacts() {
+	    ContentResolver cr = getContentResolver();
+	    Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+	           null, null, null, null);
+	    
+	    if (cur.getCount() > 0) {
+	       while (cur.moveToNext()) {
+	           String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+	           String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+	           if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+	               // get the phone number
+	               Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+	                                      ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+	                                      new String[]{id}, null);
+	               while (pCur.moveToNext()) {
+	                     String phone = pCur.getString(
+	                            pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+	                     String numberType = pCur.getString(pCur.getColumnIndex(
+	                    		 ContactsContract.CommonDataKinds.Phone.TYPE));
+	                     
+	                     ContactMap NamePhoneType = new ContactMap();
+	
+	                     NamePhoneType.put("Name", name);
+	                     NamePhoneType.put("Phone", phone);
+	
+	                     if(numberType.equals("0"))
+	                    	 NamePhoneType.put("Type", "Work");
+	                     else if(numberType.equals("1"))
+	                    	 NamePhoneType.put("Type", "Home");
+	                     else if(numberType.equals("2"))
+	                    	 NamePhoneType.put("Type",  "Mobile");
+	                     else
+	                    	 NamePhoneType.put("Type", "Other");
+	
+	                     //Then add this map to the list.
+	                     mPeopleList.add(NamePhoneType);
+	                     mNames.add(name);
+	               }
+	               pCur.close();
+	           }
+	       }
+	    }
+	    cur.close();
+	    //startManagingCursor(cur);
+	    isSelected = new boolean[mPeopleList.size()];
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		this.task = new DownloadImageTask();
 	    // Get the layout inflater
 		setContentView(R.layout.dialog_makegroup);
@@ -105,6 +149,7 @@ public class NewGroupActivity extends Activity {
 		this.btnCancelGroup = (Button) findViewById(R.id.btnCancelGroup);	
 		this.groupName = (EditText) findViewById(R.id.groupName);
 		this.picURL = (EditText) findViewById(R.id.picURL);
+		errorMsg= (TextView) findViewById(R.id.group_error); 
 		// get user
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
 		this.email = prefs.getString("app_email", null);
@@ -116,14 +161,15 @@ public class NewGroupActivity extends Activity {
         
         readContacts();
         
-        name_Val = (String[]) c_Name.toArray(new String[c_Name.size()]);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, name_Val);
+        mAdapter = new SimpleAdapter(this, mPeopleList, R.layout.custautocomplete ,new String[] { "Name", "Phone" , "Type" }, new int[] { R.id.ccontName, R.id.ccontNo, R.id.ccontType });
         contactView.setThreshold(1);
-        contactView.setAdapter(adapter);
+        contactView.setAdapter(mAdapter);
+        
         
 		this.uf = new UserFunctions();
 		this.image = (ImageView) findViewById(R.id.imageView1);
 		
+		// buttons
 		btnConfirmGroup.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -139,6 +185,83 @@ public class NewGroupActivity extends Activity {
 		});
 	}
 	
+	private String[] getInvitees() {
+		ArrayList<String> numbers = new ArrayList<String>();
+		// add the ones with isSelected == true
+		for (int i = 0; i < isSelected.length; i++)
+			if (isSelected[i])
+				numbers.add(PhoneNumberUtils.extractNetworkPortion(mPeopleList.get(i).get("Phone")));
+		
+		String[] invitees = contactView.getText().toString().split(",\\s*"); //
+		for (String name : invitees) {
+			// get that contact's number(s)
+			int index = mNames.indexOf(name);
+			// found the name
+			if (index >= 0) {
+				// invite if not already invited
+				int tmp = index;
+				while (tmp < mPeopleList.size()) {
+					Map<String, String> obj = mPeopleList.get(tmp);
+					String foundName = obj.get("Name");
+					if (!foundName.equals(name)) {
+						numbers.add(mPeopleList.get(index).get("Phone"));
+						break;
+					}
+					if (isSelected[tmp]) break;
+					tmp++;
+				}
+				if (tmp == mPeopleList.size())
+					numbers.add(PhoneNumberUtils.extractNetworkPortion(mPeopleList.get(index).get("Phone")));
+			} else {
+				String inputAsPhone = PhoneNumberUtils.convertKeypadLettersToDigits(name);
+				inputAsPhone = PhoneNumberUtils.stripSeparators(inputAsPhone); 
+				if (PhoneNumberUtils.isWellFormedSmsAddress(inputAsPhone))
+					numbers.add(inputAsPhone);
+			}
+		}
+		return (String[])numbers.toArray();
+	}
+	
+	public void inviteMembers(int groupId, String[] invitees) {
+		// post it all
+		JSONObject grpJson = uf.inviteMembers(email, token, groupId, invitees);
+		try {
+			Log.d("toString", grpJson.toString());
+			String ourStatus = "";
+			if (grpJson.has(STATUS)) {
+				ourStatus = grpJson.getString(STATUS);
+			}
+			//Log.d("JSON status", ourStatus);
+			
+			if (!ourStatus.equals(ERROR)) {
+				return;
+			} else {
+				this.show("Failure to invite members.");
+				thread=  new Thread(){
+			        @Override
+			        public void run(){
+			            try {
+			                synchronized(this){
+			                    wait(3000);
+			                }
+			            }
+			            catch(InterruptedException ex){                    
+			            }
+
+			            // TODO              
+			        }
+			    };
+
+			    thread.start();
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			// print message?
+		}
+	
+	}
+	
 	public void confirmGroup() {
  	    //UserFunctions userFunctions = new UserFunctions();
  	    // pull the strings from the edittexts. send groupname to server. get picture using url.
@@ -150,6 +273,11 @@ public class NewGroupActivity extends Activity {
 			startActivity(intent);
 			finish();
 		}
+		// some invitees are not valid.
+		String[] invitees = getInvitees();
+		if (invitees == null)
+			return;
+		
 		JSONObject grpJson = uf.createGroup(email, token, groupName.getText().toString(), picURL.getText().toString());
 		try {
 			Log.d("toString", grpJson.toString());
@@ -171,14 +299,16 @@ public class NewGroupActivity extends Activity {
 				groupName.setHint("Enter group name");
 				picURL.setText("");
 				picURL.setHint("Enter image URL");
- 	   
+				
+				inviteMembers(Integer.parseInt(grpJson.getString("group_id")), invitees);
+				
 				// then end the activity, returning to MainActivity
 				Intent intent = new Intent(this, MainActivity.class);
 				startActivity(intent);
 				finish();
 			} else {
 				// Inform user that group creation has failed?
-				// this.show("Group creation failed. Please try again.");
+				this.show("Group creation failed. Please try again.");
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
@@ -318,5 +448,17 @@ public class NewGroupActivity extends Activity {
 		    return;
 	    }
     } 
-	
+
+    /**
+     * This method is a hook for background threads and async tasks that need to update the UI.
+     * It does this by launching a runnable under the UI thread.
+     */
+    public void show(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+            	errorMsg.setText(message);
+            }
+        });
+    }
 }
